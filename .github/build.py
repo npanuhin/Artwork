90b4b96e95d09eba5d3c432119b3b31b703d43f5
Panuhin Nikita <79215987151@mail.ru>
2020-01-19 13:49:03 +0300

    Build refactoring

--- .github/build.py
+++ .github/build.py
@@ -17,6 +17,7 @@ def make_path(*paths: List[str]) -> str:
 
 
 def checkGithubPages(image: str) -> bool:
+    '''Checks whether to create a page for an image'''
     if not os.path.exists(make_path(root_path, "SVG", image, "src/conf.json")):
         return False
     with open(make_path(root_path, "SVG", image, "src/conf.json"), 'r', encoding="utf-8") as conf_file:
@@ -44,15 +45,12 @@ def githubPages() -> None:
     images = []
 
     for image in os.listdir(make_path(rp, "SVG")):
-        if not checkGithubPages(image):
-            continue
-        images.append("-   [{name}](./{url} \"See {name} SGV image\")".format(name=image, url=image.replace(' ', '%20')))
-
-    svg_md += "\n\n" + "\n".join(images)
+        if checkGithubPages(image):
+            images.append("-   [{name}](./{url} \"See {name} SGV image\")".format(name=image, url=image.replace(' ', '%20')))
 
     # Save
     with open(make_path(rp, "SVG/README.md"), 'w', encoding='utf-8') as file:
-        file.write(svg_md)
+        file.write(svg_md + "\n\n" + "\n".join(images))
 
     print("Created README.md with list of SVG images")
 
@@ -65,42 +63,27 @@ def githubPages() -> None:
         image_colored_template = file.read().strip()
 
     for image in os.listdir(make_path(rp, "SVG")):
-        # Many checks
         if not checkGithubPages(image):
             continue
+
         with open(make_path(rp, "SVG", image, "src/conf.json"), 'r', encoding="utf-8") as conf_file:
             conf_data = loads(conf_file.read(), encoding="utf-8")['github_pages']
-
-        if "name" not in conf_data or not isinstance(conf_data["name"], str):
-            print("Warning! Can not handle due to invalid parameters: \"" + image + "\"")
-            continue
 
         has_not_colored_version = (image + ".svg" in os.listdir(make_path(rp, "SVG", image)))
         has_colored_version = (image + ".colored.svg" in os.listdir(make_path(rp, "SVG", image)))
 
-        if has_not_colored_version and "name" not in conf_data or not isinstance(conf_data["name"], str):
-            print("Warning! Can not handle due to invalid parameters: \"" + image + "\"")
-            continue
-        if has_colored_version and "colored_name" not in conf_data or not isinstance(conf_data["colored_name"], str):
-            print("Warning! Can not handle due to invalid parameters: \"" + image + "\"")
-            continue
-
-        if not os.path.exists(make_path(rp, "SVG", image, "src/template.md")) or not os.path.exists(make_path(rp, "SVG", image, "src/description.md")):
-            print("Warning! Can not handle \"" + image + "\". No such file or directory.")
-            continue
-
         with open(make_path(rp, "SVG", image, "src/template.md"), 'r', encoding="utf-8") as file:
-            image_template = file.read()
-        if image_template:
-            image_template = "\n" + image_template + "\n"
+            image_template = file.read().strip()
+            if image_template:
+                image_template = "\n" + image_template + "\n"
         with open(make_path(rp, "SVG", image, "src/description.md"), 'r', encoding="utf-8") as file:
-            image_description = file.read()
-        if image_description:
-            image_description = "\n" + image_description + "\n"
+            image_description = file.read().strip()
+            if image_description:
+                image_description = "\n" + image_description + "\n"
 
         image_md = image_md_template
-
         image_colored_template_loc = image_colored_template
+
         if has_colored_version and has_not_colored_version:
             image_colored_template_loc += "\n"
 
@@ -127,7 +110,7 @@ def renderAll() -> None:
     '''Renders all SVG files'''
     print("Rendering files...")
     rp = root_path
-    re_find = re.compile(r"^\d+\)[A-Za-zА-Яа-я0-9 \/\\\t\-\+=\*\)\(\.<>\?\,\%\$\#\@\!\^\;\"\'\`\~]+\((\d+)x(\d+)\)\.png$")
+    re_find_not_colored = re.compile(r"^\d+\)[A-Za-zА-Яа-я0-9 \/\\\t\-\+=\*\)\(\.<>\?\,\%\$\#\@\!\^\;\"\'\`\~]+\((\d+)x(\d+)\)\.png$")
     re_find_colored = re.compile(r"^\d+\)[A-Za-zА-Яа-я0-9 \/\\\t\-\+=\*\)\(\.<>\?\,\%\$\#\@\!\^\;\"\'\`\~]+\((\d+)x(\d+)\)\.colored\.png$")
 
     for image in os.listdir(make_path(rp, "SVG")):
@@ -136,23 +119,23 @@ def renderAll() -> None:
 
         with open(make_path(rp, "SVG", image, "src/conf.json"), 'r', encoding="utf-8") as conf_file:
             conf_data = loads(conf_file.read(), encoding="utf-8")
-
-        if "render_sizes" not in conf_data:
-            continue
-
-        sizes = conf_data["render_sizes"]
+            if "render_sizes" not in conf_data:
+                continue
+            sizes = conf_data["render_sizes"]
 
         render_path = make_path(rp, "SVG", image, "render")
 
+        # Create render directory if it does not exist
         if not os.path.exists(render_path):
             os.mkdir(render_path)
 
+        # Delete all files if "update_old_files" set to True
         if update_old_files:
             for file in os.listdir(render_path):
                 os.remove(make_path(render_path, file))
 
         for svg_filename in os.listdir(make_path(rp, "SVG", image)):
-            if os.path.splitext(svg_filename)[1] != ".svg":
+            if os.path.splitext(svg_filename)[1] != ".svg":  # Finding SVG files
                 continue
 
             svg_basename = os.path.splitext(svg_filename)[0]
@@ -160,18 +143,17 @@ def renderAll() -> None:
             output_path = make_path(render_path, svg_basename + ".png")
 
             # List of sizes, that are already rendered
-            not_render = []
+            rendered = []
             if not update_old_files:
-                if svg_basename.endswith(".colored"):
-                    for filename in os.listdir(render_path):
+                for filename in os.listdir(render_path):
+                    if svg_basename.endswith(".colored"):
                         matches = re_find_colored.match(filename)
-                        if matches:
-                            not_render.append([int(matches.group(1)), int(matches.group(2))])
-                else:
-                    for filename in os.listdir(render_path):
-                        matches = re_find.match(filename)
-                        if matches:
-                            not_render.append([int(matches.group(1)), int(matches.group(2))])
+                    else:
+                        matches = re_find_not_colored.match(filename)
+
+                    if matches:
+                        rendered.append([None, int(matches.group(2))])
+                        rendered.append([int(matches.group(1)), None])
 
             if svg_basename.endswith(".colored"):
                 output_name = "{i}) {name} ({rw}x{rh}).colored.png"
@@ -180,46 +162,41 @@ def renderAll() -> None:
                 output_name = "{i}) {name} ({rw}x{rh}).png"
 
             # SVG sizes
-            with open(svg_path, 'r', encoding='utf-8') as svg_file_data:
-                svg_data = svg_file_data.read()
+            with open(svg_path, 'r', encoding='utf-8') as svg_data_file:
+                svg_data = svg_data_file.read()
             image_sizes = BeautifulSoup(svg_data, "lxml").find("svg").get('viewbox').split()
             width = int(image_sizes[2]) - int(image_sizes[0])
             height = int(image_sizes[3]) - int(image_sizes[1])
 
-            index_length = len(str(len(sizes)))
-            index = 0
+            total_count = len(str(len(sizes)))
+            count = 0
             for x, y in sizes.values():
-                index += 1
+                count += 1
                 if not (isinstance(x, str) ^ isinstance(y, str) and (x is None) ^ (y is None)):
                     print("Warning! Can not handle due to invalid parameters: \"" + svg_path + "\"")
                     continue
 
-                if y is None:
-                    x = literal_eval(x.format(w=width, h=height))
-                else:
+                if x is None:
                     y = literal_eval(y.format(w=width, h=height))
+                else:
+                    x = literal_eval(x.format(w=width, h=height))
 
-                flag = False
-                for test_x, test_y in not_render:
-                    if (y is None and test_x == x) or (x is None and test_y == y):
-                        flag = True
-                        break
-                if flag:
+                if [x, y] in rendered:
                     continue
 
-                if y is None:
-                    # Render using width
-                    render.renderSvg(svg_path, output_path, width=x)
-                else:
+                if x is None:
                     # Render using height
                     render.renderSvg(svg_path, output_path, height=y)
+                else:
+                    # Render using width
+                    render.renderSvg(svg_path, output_path, width=x)
 
                 # Result image sizes
                 result_width, result_height = Image.open(output_path).size
 
                 # Save with correct name
                 os.rename(output_path, make_path(render_path, output_name.format(
-                    i=str(index).zfill(index_length), name=svg_basename, rw=result_width, rh=result_height
+                    i=str(count).zfill(total_count), name=svg_basename, rw=result_width, rh=result_height
                 )))
 
                 print("{name} (width: {w}, height: {h}) rendered as png image (width: {rw}, height: {rh})".format(
